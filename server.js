42f5a89df090573075a3eda4d54857504e389d68
Sujan Timalsina <sujantimalsina333@gmail.com>
2025-06-24 23:47:11 +0545

    Set up EJS as the template engine and configure static file serving

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const connectDb = require("./db/db");
 const dotenv = require("dotenv");
+const path=require("path")
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+//template engine
+app.set("views",path.join(__dirname,"/views"));
+app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/files", require("./routes/files.route"));
+app.use("/files", require("./routes/show.route"));
 
 
 const startServer = async () => {
